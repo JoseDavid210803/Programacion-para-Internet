c7d98eb2d400fec516b05728cbe28b4c6bc4d7d2
JoseDavid210803 <jose.castellanos4652@alumnos.udg.mx>
2023-04-24 20:56:24 -0600

    Termine la pagina de detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,18 @@
 /* Validate if constant exists */
 
 /*=============== IMAGE GALLERY ===============*/
+function imgGallery() {
+  const mainImg = document.querySelector('.details__img'),
+  smallImg = document.querySelectorAll('.details__small-img');
 
+  smallImg.forEach((img) => {
+    img.addEventListener('click', function() {
+      mainImg.src = this.src;
+    })
+  })
+}
+
+imgGallery();
 /*=============== SWIPER CATEGORIES ===============*/
 var swiperCategories = new Swiper(".categories__container", {
     spaceBetween: 24,
